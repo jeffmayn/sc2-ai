6e983b3362afa495f7db31d7ccf9857e49d9592a
jeffmayn <jeffmayn>
2018-10-02 22:45:43 +0200

    when supply depot below 10 left, a SCV will build new supply depot

--- terran/terranAI.py
+++ terran/terranAI.py
@@ -6,16 +6,25 @@ from sc2.player import Human
 
 class terranAI(sc2.BotAI):
     async def on_step(self, iteration):
+        await self.distribute_workers()
+        await self.buildSCVs()
+        await self.buildSupplyDepot()
+
+    async def buildSCVs(self):
         # declare command center cc as first one build
         cc = self.units(COMMANDCENTER)
         cc = cc.first
 
-        # get SCVs to start gathering minerals
-        await self.distribute_workers()
-
         # train SCVs
         if self.can_afford(SCV) and self.workers.amount < 22 and cc.noqueue:
             await self.do(cc.train(SCV))
+
+    async def buildSupplyDepot(self):
+        if self.supply_left < 10 and not self.already_pending(SUPPLYDEPOT):
+            scv = self.units(SCV).ready
+            if scv.exists:
+                if self.can_afford(SUPPLYDEPOT):
+                    await self.build(SUPPLYDEPOT, near=scv.first)
 
 def main():
     sc2.run_game(sc2.maps.get("AcidPlantLE"),
